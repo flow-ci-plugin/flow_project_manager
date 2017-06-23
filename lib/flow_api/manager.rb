6cf558fecbec450c25bd3cfe0783e909de0170ee
atpking <atpking@gmail.com>
2017-06-23 15:14:10 +0800

    add test file

--- lib/flow_api/manager.rb
+++ lib/flow_api/manager.rb
@@ -6,24 +6,22 @@ module FlowApi
   class Rest < DefaultRest
     class << self
       def basic_url
-        ENV["FLOW_API_BASIC_URL"] || "http://api-lyon.flow.ci"
+        ENV["FLOW_DOMAIN"] || "http://api-lyon.flow.ci"
       end
 
       def base_params
         {
-          api_token: ENV["FLOW_API_PROJECT_TOKEN"]
+          api_token: ENV["FLOW_PROJECT_API_TOKEN"]
         }
       end
-     
-     def basic_options
-       @tmp ||= super.merge(other_base_execute_option: {log: Logger.new(STDOUT)})
-     end
 
+      def basic_options
+        @tmp ||= super.merge(other_base_execute_option: { log: Logger.new(STDOUT) })
+      end
 
       def send_log_json(project_id, job_id, json_str)
-        path = "/jobs/#{job_id}/log_json_str"
-        post(path, project_id: project_id,
-                   json_str: json_str)
+        path = "/jobs/#{job_id}/upload_test_log_details"
+        patch(path, project_id: project_id, json_str: json_str)
       end
     end
   end
