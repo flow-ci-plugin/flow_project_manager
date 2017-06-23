b28d020a27e9cae31b330a37b2f9f034fc144056
atpking <atpking@gmail.com>
2017-06-23 10:58:19 +0800

    add config

--- lib/flow_api/manager.rb
+++ lib/flow_api/manager.rb
@@ -6,7 +6,7 @@ module FlowApi
   class Rest < DefaultRest
     class << self
       def basic_url
-        ENV["FLOW_API_BASIC_URL"] || "http://api.flow.ci"
+        ENV["FLOW_API_BASIC_URL"] || "http://api-lyon.flow.ci"
       end
 
       def base_params
@@ -14,6 +14,11 @@ module FlowApi
           api_token: ENV["FLOW_API_PROJECT_TOKEN"]
         }
       end
+     
+     def basic_options
+       @tmp ||= super.merge(other_base_execute_option: {log: Logger.new(STDOUT)})
+     end
+
 
       def send_log_json(project_id, job_id, json_str)
         path = "/jobs/#{job_id}/log_json_str"
@@ -26,7 +31,7 @@ module FlowApi
     class << self
       def junit_file_to_hash(junit_path)
         return {} unless File.exist? junit_path
-        log_xml = File.read(file_path)
+        log_xml = File.read(junit_path)
         Crack::XML.parse(log_xml)
       end
 
